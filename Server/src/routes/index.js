a2552f402150b4dbcb1566d3ba6eafe1558ee687
pnzaou <perrinemmanuelnzaou@gmail.com>
2024-12-20 14:25:50 +0000

    Mise à jour des routes

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,17 +1,39 @@
 const express = require("express")
-const { signIn, signUp } = require("../controllers/user.controller")
+const { signIn, signUp, toggleFav, toggleSub, getAuthUserDetails, getUserDetails } = require("../controllers/user.controller")
 const { uploadSingle, uploadMultiple } = require("../middlewares/multer.middleware")
 const verifToken = require("../middlewares/verifToken.middleware")
 const { createPost, addToDraft } = require("../controllers/post.controller")
+const { addComment, updateComment, deleteComment } = require("../controllers/comment.controller")
+const { toggleLikedPost, toggleLikedComment } = require("../controllers/like.controller")
 const router = express.Router()
 
+//USER
 router.post("/api/users/signUp", uploadSingle, signUp)
 router.post("/api/users/signIn", signIn)
+router.get("/api/users/auth-details", verifToken, getAuthUserDetails)
+router.get("/api/users/details", getUserDetails)
+router.patch("/api/users/toggle-favorites", verifToken, toggleFav)
+router.patch("/api/users/toggle-subscriptions", verifToken, toggleSub)
 
 
+//POST
 router.route("/api/posts")
     .post(verifToken, uploadMultiple, createPost)
 router.post("/api/posts/add-to-draft", verifToken, uploadMultiple, addToDraft)
 router.put("/api/post/:id")
+
+
+//COMMENT
+router.post("/api/comments")
+    .post(verifToken, addComment)
+router.post("/api/comment/:commentId")
+    .put(verifToken, updateComment)
+    .delete(verifToken, deleteComment)
+
+
+//LIKE
+router.post("/api/likes/post", verifToken, toggleLikedPost)
+router.post("/api/likes/comment", verifToken, toggleLikedComment)
+
 
 module.exports = router
